d9d790bda333517e44e0fe1787cf7df661b5a711
ttexan1 <tetsuji.higashi@geniee.co.jp>
2023-01-08 20:20:57 +0900

    make first test

---
+++ src/arithmetic.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+class Arithmetic {
+
+    public function add($x,$y) {
+        return ($x+$y);
+    }
+    public function subtract($x,$y) {
+        return ($x-$y);
+    }
+    public function multiply($x,$y) {
+        return ($x*$y);
+    }
+    public function divide($x,$y) {
+        return ($x/$y);
+    }
+}
+?>
